26653a70d99692c4966a2402953901d636a99de9
yashank <yaashank.mittal@gmail.com>
2022-01-09 20:27:23 +0530

    changes

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const expresslayouts = require('express-ejs-layouts');
 const path = require('path');
 
 
+//set Template engine
+app.use(expresslayouts);
+app.set('views',path.join(__dirname,'/resources/views'))
+app.set('view engine','ejs');
+
+
+
 
 //Assets
 app.use(express.static('public'))
@@ -13,13 +20,17 @@ app.get('/',(req,res)=>{
     res.render('home')
 })
 
+app.get('/cart',(req,res)=>{
+    res.render('customers/cart');
+})
 
+app.get('/login',(req,res)=>{
+    res.render('auth/login')
+})
 
-//set Template engine
-app.use(expresslayouts);
-app.set('views',path.join(__dirname,'/resources/views'))
-app.set('view engine','ejs');
-
+app.get('/register',(req,res)=>{
+    res.render('auth/register')
+})
 
 const PORT= process.env.PORT || 8080;
 app.listen(PORT,()=>{console.log(`Port is running on ${PORT}`)});
